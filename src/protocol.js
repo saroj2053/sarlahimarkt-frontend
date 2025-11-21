26a7a57a0865223958692553d35de60286ff756d
Saroj Sah <sarojsaroj390@gmail.com>
2025-11-21 08:43:12 +0100

    Updated API endpoint to use production URL

--- src/protocol.js
+++ src/protocol.js
@@ -1,5 +1,5 @@
-// export const api = "https://sarlahimarkt.onrender.com/api/v1";
-export const api = "http://localhost:5000/api/v1";
+export const api = "https://sarlahimarkt.onrender.com/api/v1";
+// export const api = "http://localhost:5000/api/v1";
 
 export const getBearerToken = () => {
   const user = JSON.parse(localStorage.getItem("userInfo"));
